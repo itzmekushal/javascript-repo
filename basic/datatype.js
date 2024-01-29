2a054ae346c93b2960cc24f9ff58c555236b300c
Kushal Lakhmapure <93693551+itzmekushal@users.noreply.github.com>
2024-01-29 06:55:54 +0000

    datatype config

---
+++ basic/datatype.js
@@ -0,0 +1,11 @@
+let number = "33"
+let conversion = Number(number)
+console.log(conversion)
+console.log(typeof conversion)
+// conversion of string to number
+
+
+let name="kushal";
+console.log(name);
+let conversion2 = Boolean(name)
+console.log(conversion2)
